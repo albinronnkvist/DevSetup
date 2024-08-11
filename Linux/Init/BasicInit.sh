cb3f94469111c798d4207653a8c1ad6d773116dc
Albin <albinronnkvist@protonmail.com>
2024-08-11 16:42:16 +0200

    Run DevFolder setup last

--- Linux/Init/BasicInit.sh
+++ Linux/Init/BasicInit.sh
@@ -6,10 +6,10 @@ read -r EMAIL
 
 export EMAIL
 
-bash ./Basics/DevFolder/Setup.sh
 bash ./Basics/Aliases/Setup.sh
 bash ./Basics/Git/Setup.sh
 bash ./Basics/Docker/Setup.sh
 bash ./Basics/Codium/Setup.sh
+bash ./Basics/DevFolder/Setup.sh
 
 echo "All tasks completed successfully. Please restart your terminal or run 'source ~/.bashrc' to apply changes."
