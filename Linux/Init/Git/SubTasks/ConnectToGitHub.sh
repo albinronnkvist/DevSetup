9654d8749ff7fbc8245cb8b9ed15849aeec6f569
Albin <albinronnkvist@protonmail.com>
2024-08-11 17:08:26 +0200

    Fix GitHub SSH connection

--- Linux/Init/Git/SubTasks/ConnectToGitHub.sh
+++ Linux/Init/Git/SubTasks/ConnectToGitHub.sh
@@ -1,38 +1,35 @@
 #!/bin/bash
 
 # Check for existing SSH keys
-if [ -f ~/.ssh/id_rsa ] && [ -f ~/.ssh/id_rsa.pub ]; then
-    echo "Existing SSH key pair found:"
-    ls -al ~/.ssh/id_rsa ~/.ssh/id_rsa.pub
+if [ -f ~/.ssh/id_ed25519.pub ]; then
+    echo "Existing SSH key pair found."
 else
-    echo "No existing SSH key pair found. Generating a new SSH key pair..."
+    echo -e "\nNo existing SSH key pair found. Generating a new SSH key pair..."
+    echo -e "\nWhen you're prompted to "Enter a file in which to save the key", you can press Enter to accept the default file location."
     
     # Generate a new SSH key pair
-    ssh-keygen -t rsa -b 4096 -C "$EMAIL"
+    ssh-keygen -t ed25519 -C "$EMAIL"
     
-    echo "New SSH key pair generated."
+    echo "N\new SSH key pair generated."
+
+    # Start the SSH agent and add the SSH key
+    echo -e "\nAdding SSH key to the SSH agent..."
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_ed25519
+    echo -e "\nSSH key added to the SSH agent."
 fi
 
-# Start the SSH agent and add the SSH key
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-echo "SSH key added to the SSH agent."
-
 # Display the SSH public key
-echo "Your SSH public key is:"
-cat ~/.ssh/id_rsa.pub
-
-echo "Please copy the above SSH public key and add it to your GitHub account."
+echo -e "\nYour SSH public key is:"
+cat ~/.ssh/id_ed25519.pub
 
 # Instructions for adding the SSH key to GitHub
-echo "To add your SSH key to GitHub:"
+echo -e "\nTo add your SSH key to GitHub:"
 echo "1. Go to https://github.com/settings/keys"
 echo "2. Click on 'New SSH key'"
 echo "3. Paste your SSH key into the 'Key' field and give it a title"
 echo "4. Click 'Add SSH key'"
 
 # Test the SSH connection to GitHub
-echo "Testing SSH connection to GitHub..."
+echo -e "\nTesting SSH connection to GitHub..."
 ssh -T git@github.com
-
-echo "If you see a message like 'Hi username! You've successfully authenticated,' then everything is set up correctly."
