6843c13ce280e6b9dd579552de36e588c63b305e
Albin <albinronnkvist@protonmail.com>
2024-08-30 15:30:31 +0200

    Add InstallClamAV to basic init

--- Linux/Init/BasicInit.sh
+++ Linux/Init/BasicInit.sh
@@ -10,6 +10,11 @@ bash ./Aliases/Setup.sh
 bash ./Git/Setup.sh
 bash ./Docker/Setup.sh
 bash ./CodeEditors/Codium/Setup.sh
+
+# Other
+bash ./Other/InstallClamAV.sh
+
+# Run DevFolder last
 bash ./DevFolder/Setup.sh
 
-echo "All tasks completed successfully. Please restart your terminal or run 'source ~/.bashrc' to apply changes."
+echo "All basic init tasks completed successfully."
