38b3fe40428bcac0cc4391b0a871d92b8d35fddf
Albin <albinronnkvist@protonmail.com>
2024-09-01 15:45:27 +0200

    Simplify Java Installation

--- Linux/Init/ProgrammingLanguages/Java/SubTasks/InstallJDK.sh
+++ Linux/Init/ProgrammingLanguages/Java/SubTasks/InstallJDK.sh
@@ -20,41 +20,6 @@ else
     fi
 fi
 
-echo "Installed JDK versions:"
-sudo update-alternatives --list java | awk -F '/' '{print $5}' | sort -u
-
-echo
-echo "Select JDK version to use: "
-sudo update-alternatives --config java
-
-echo
-echo "JDK version currently in use: "
-java -version
-
-echo
-echo "View all locally installed Java packages by running: sudo dpkg -l | grep 'jdk\|jre'"
-#!/bin/bash
-
-DEFAULT_JDK_VERSION="17"
-JDK_VERSION=${1:-$DEFAULT_JDK_VERSION}
-
-install_jdk() {
-    sudo apt update && sudo apt install -y openjdk-$JDK_VERSION-jdk
-    echo "JDK $JDK_VERSION installed"
-}
-
-if ! command -v java &> /dev/null; then
-    install_jdk
-else
-    INSTALLED_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}' | awk -F '.' '{print $1}')
-    if [ "$INSTALLED_VERSION" == "$JDK_VERSION" ]; then
-        echo "JDK $JDK_VERSION is already installed"
-    else
-        echo "Different JDK version ($INSTALLED_VERSION) is installed. Installing JDK $JDK_VERSION."
-        install_jdk
-    fi
-fi
-
 echo
 echo "Select JDK version to use: "
 sudo update-alternatives --config java
