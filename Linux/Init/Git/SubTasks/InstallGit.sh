afe3f979a8fd42f8386939c40bafd9310b85871d
Albin <albinronnkvist@protonmail.com>
2024-09-06 10:15:25 +0200

    Add install github CLI

--- Linux/Init/Git/SubTasks/InstallGit.sh
+++ Linux/Init/Git/SubTasks/InstallGit.sh
@@ -2,7 +2,9 @@
 
 # Install Git if it's not already installed
 if ! command -v git &> /dev/null; then
-    sudo apt update && sudo apt install -y git
+    sudo apt update
+    sudo apt install -y git
+    sudo apt install -y gh
     echo "Git installed"
 else
     echo "Git is already installed"
